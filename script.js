1f1f112358d1dfde000402845ea94e7df527da54
Jahongir <nigoranosirova232@gmail.com>
2022-12-27 12:38:49 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// const favouriteColor = [];
+// favouriteColor[0] = prompt("What's your favourite color #1", "");
+// favouriteColor[1] = prompt("What's your favourite color #2", "");
+// favouriteColor[2] = prompt("What's your favourite color #3", "");
+
+// console.log(favouriteColor);
+
+// const chanel = prompt("Eng yoqtirgan yutub kanaliz?", " ");
+// console.log(`https://youtobe.com/${chanel}`);
+
+const isAge = true;
+const isClose = true;
+const isChecked = false;
+
+console.log(isAge && isClose && !isChecked);
